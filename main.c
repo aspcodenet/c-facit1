1608373e6d39d44dee993b6f398f1d375f5bca6e
Stefan Holmberg <stefan.holmberg@systementor.se>
2022-12-28 09:26:22 +0100

    first commit

---
+++ main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+void main(){
+    // 2a
+    // printf("Hej jag heter Stefan\n");
+    // printf("Testgatan 12\n");
+    // printf("111 55  Teststad\n");
+    //2b
+    printf("Hej jag heter Stefan\nTestgatan 12\n111 55  Teststad\n");
+
+}
