f30038f35db8fc2b655843497d69abeadbf813ed
Stefan Holmberg <stefan.holmberg@systementor.se>
2022-12-28 10:59:27 +0100

    lab5

--- main.c
+++ main.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 
 void main(){
-    // 2a
-    // printf("Hej jag heter Stefan\n");
-    // printf("Testgatan 12\n");
-    // printf("111 55  Teststad\n");
-    //2b
-    printf("Hej jag heter Stefan\nTestgatan 12\n111 55  Teststad\n");
-
+    // char, short int, int, long, float, double
+    printf("char\t%d\t%d\n", sizeof(char), sizeof(char) * 8);
+    printf("short\t%d\t%d\n", sizeof(short), sizeof(short) * 8);
+    printf("int\t%d\t%d\n", sizeof(int), sizeof(int) * 8);
+    printf("long\t%d\t%d\n", sizeof(long), sizeof(long) * 8);
+    printf("llong\t%d\t%d\n", sizeof(long long), sizeof(long long) * 8);
+    printf("float\t%d\t%d\n", sizeof(float), sizeof(float) * 8);
+    printf("double\t%d\t%d\n", sizeof(double), sizeof(double) * 8);
 }
