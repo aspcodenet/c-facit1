22399adf244f5e44e6e0cd2bccb76bcf5b4a7891
Stefan Holmberg <stefan.holmberg@systementor.se>
2022-12-28 09:55:01 +0100

    lab3

--- main.c
+++ main.c
@@ -1,11 +1,20 @@
 #include <stdio.h>
 
 void main(){
-    // 2a
-    // printf("Hej jag heter Stefan\n");
-    // printf("Testgatan 12\n");
-    // printf("111 55  Teststad\n");
-    //2b
-    printf("Hej jag heter Stefan\nTestgatan 12\n111 55  Teststad\n");
+    int days;
+    int pricePerNight;
+    int totalPrice;
+
+    printf("Vad kostar hotellet per natt? ");
+    scanf(" %d", &pricePerNight);
+
+    printf("Hur många nätter?");
+    scanf(" %d", &days);
+
+    totalPrice = days * pricePerNight;
+    printf("Det blir %dkr\n", totalPrice);
+
+    //printf("Det blir %dkr\n", days * pricePerNight);
+
 
 }
