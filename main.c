3156783ad660998ba82087c02f57bdc974fdc394
Stefan Holmberg <stefan.holmberg@systementor.se>
2022-12-28 11:10:38 +0100

    lab6

--- main.c
+++ main.c
@@ -1,11 +1,18 @@
 #include <stdio.h>
 
+const double pi = 3.14159265358979323846;
+
 void main(){
-    // 2a
-    // printf("Hej jag heter Stefan\n");
-    // printf("Testgatan 12\n");
-    // printf("111 55  Teststad\n");
-    //2b
-    printf("Hej jag heter Stefan\nTestgatan 12\n111 55  Teststad\n");
+    float radie;
+    float omkrets;
+
+    printf("Ange omkrets:"); 
+    scanf(" %f", &radie);
+
+    omkrets = 2 * radie * pi;
+    printf("Omkretsen blev:%f\n", omkrets);
+
+    //Tre decimaler
+    printf("Omkretsen blev:%.3f\n", omkrets);
 
 }
